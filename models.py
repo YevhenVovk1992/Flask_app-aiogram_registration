dea6787e331c35349e905e01c2c3c393b58c0dd3
YevhenVovk <vovk.valki@ukr.net>
2023-03-29 20:24:09 +0300

    Use Alembic for migrations
    init alembic;
    create migration;

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Numeric, Text
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class User(Base):
+    """
+    Create user's model
+    """
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    name = Column(String(50), unique=True)
+    email = Column(String(100), unique=True)
+    password = Column(String(100))
+    telegram_name = Column(String(100), unique=True)
+    telephone = Column(String(100), unique=True)
+
+    def __str__(self):
+        return f'{self.id}-{self.name}'
